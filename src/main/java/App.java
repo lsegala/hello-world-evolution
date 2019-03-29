6a51c6e65cfdf3b6fef98d24be69969758d3b574
leonardo <leonardo.segala@gmail.com>
2019-03-29 11:31:17 -0300

    evoluindo aplicação para mostrar o nome

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println("Hello world"+(args.length > 0? ", "+args[0] : "")+"!");
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -6,17 +8,31 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class AppTest {
+    private static final String EOL = System.getProperty("line.separator");
+    private PrintStream console;
+    private ByteArrayOutputStream bytes;
+
+    @Before
+    public void setUp(){
+        bytes = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @Test
+    public void testOutputHelloWorldWithNull(){
+        App.main(new String[]{});
+        assertEquals("Hello world!"+EOL, bytes.toString());
+    }
+
     @Test
     public void testOutputHelloWorld(){
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        PrintStream console = System.out;
-        try {
-            System.setOut(new PrintStream(bytes));
-            String[] args = new String[1];
-            App.main(args);
-        }finally {
-            System.setOut(console);
-        }
-        assertEquals("Hello world!\n", bytes.toString());
+        App.main(new String[]{"Leonardo"});
+        assertEquals("Hello world, Leonardo!"+EOL, bytes.toString());
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(console);
     }
 }
